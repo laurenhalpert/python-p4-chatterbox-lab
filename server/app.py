577d4753c5725b322e09adada1ad48aa33ac6ea9
Lauren <halpertln@gmail.com>
2023-07-06 10:15:58 -0600

    mostly done lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,55 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = Message.query.order_by('created_at').all()
+        messages_serialized = [message.to_dict() for message in messages]
+        response = make_response(
+            jsonify(messages_serialized),
+            200
+        )
+        response.headers['Content-Type'] = 'application/json'
+    elif request.method == "POST":
+        data = request.get_json()
+        new_message = Message(
+            body = data['body'],
+            username = data['username']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        message_dict = new_message.to_dict()
+        response = make_response(
+            jsonify(message_dict),
+            201
+        )
+        response.headers['Content-Type'] = 'application/json'
+    return response
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods = ["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
-
+    message = Message.query.filter_by(id=id).first()
+    if request.method == "PATCH":
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+        db.session.add(attr)
+        db.session.commit()
+        message_dict = message.to_dict()
+        response = make_response(
+            jsonify(message_dict),
+            200
+        )
+        response.headers['Content-Type'] = 'application/json'
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        response = make_response(
+            jsonify({'deleted': True}),
+            200,
+        )
+        response.headers['Content-Type'] = 'application/json'
+    return response
 if __name__ == '__main__':
     app.run(port=5555)
